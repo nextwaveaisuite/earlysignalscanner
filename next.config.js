b98fd3ccd2eaa5133fda48f61d0915fc2b3fb38e
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-02 20:41:47 +1000

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { serverActions: { allowedOrigins: ["*"] } }
+  experimental: { serverActions: { allowedOrigins: ["*"] } },
+  webpack: (config) => {
+    // Allow imports like "@/components/..." and "@/lib/..."
+    config.resolve.alias['@'] = __dirname;
+    return config;
+  },
 };
+
 module.exports = nextConfig;
