a60d12d53be216d8ca3f9ba755848d163bf729e7
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-02 17:25:05 +1000

    Delete scripts/lib directory

--- scripts/lib/db.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supa = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!,
-  { auth: { persistSession: false } }
-);
