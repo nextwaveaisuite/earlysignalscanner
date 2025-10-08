d9cc772e3d83b6f41f702e43a0495bd1b87e760e
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-08 15:56:29 +1000

    risk_place.ts

--- pages/api/risk/risk_place.ts
+++ pages/api/risk/risk_place.ts
@@ -1,2 +1,7 @@
+// pages/api/trade/risk_place.ts
 import type { NextApiRequest, NextApiResponse } from "next";
-export default function handler(_req: NextApiRequest, res: NextApiResponse){ res.status(200).json({ placed: false }); }
+import riskPlaceHandler from "../risk/risk_place";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return (riskPlaceHandler as any)(req, res);
+}
