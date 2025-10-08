87c5791dfaf64da36508bd682e895d1419ef3da6
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-08 15:53:53 +1000

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true },
+  reactStrictMode: true
+  // Note: In Next 14, the App Router is the default; no experimental.appDir needed.
 };
+
 module.exports = nextConfig;
