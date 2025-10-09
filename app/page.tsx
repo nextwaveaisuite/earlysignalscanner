c3cf73ea6863a7f597dca238071a9ff8b0c93541
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-09 10:51:41 +1000

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,2 @@
 import { redirect } from "next/navigation";
-export const dynamic = "force-dynamic";
-export default function Home() {
-  redirect("/beginner");
-}
+export default function Home() { redirect("/beginner"); }
