5260b5a608339a99cee1230e8f9d8c5ad534edb7
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-28 15:04:25 +1000

    env.ts

---
+++ lib/env.ts
@@ -0,0 +1,12 @@
+export const SUPABASE_URL =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  process.env.SUPABASE_URL ||
+  "";
+
+export const SUPABASE_SERVICE_ROLE_KEY =
+  process.env.SUPABASE_SERVICE_ROLE_KEY ||
+  process.env.SUPABASE_SERVICE_KEY ||
+  "";
+
+export const NEXT_PUBLIC_SITE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL || "";
